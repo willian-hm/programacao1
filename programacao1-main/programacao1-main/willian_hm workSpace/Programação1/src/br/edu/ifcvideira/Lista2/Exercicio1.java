1d4ba261ffdd850addfb4d94ca6351a95b30577c
willian <34694258+willian-hm@users.noreply.github.com>
2024-03-25 14:02:34 +0000

    a

---
+++ programacao1-main/programacao1-main/willian_hm workSpace/Programação1/src/br/edu/ifcvideira/Lista2/Exercicio1.java
@@ -0,0 +1,26 @@
+package br.edu.ifcvideira.Lista2;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner in =  new Scanner(System.in);
+
+        int num;
+
+        System.out.println("Informe o número:");
+        num = Integer.parseInt(in.nextLine());
+
+        if (num > 0) {
+            System.out.println("É positivo");
+        }
+        else if (num < 0) {
+            System.out.println("É negativo");
+        } 
+        else {
+            System.out.println("É zero");
+        }
+
+        in.close();
+    }
+}
